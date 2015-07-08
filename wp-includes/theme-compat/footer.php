06dae42ad666198dba7f739a8ac874195406aaae
CHAU HOANG THIEN AN <kennyfans999@gmail.com>
2015-07-08 12:04:33 +0700

    update google

--- wp-includes/theme-compat/footer.php
+++ wp-includes/theme-compat/footer.php
@@ -26,15 +26,21 @@ _deprecated_file( sprintf( __( 'Theme without %1$s' ), basename(__FILE__) ), '3.
 <?php /* "Just what do you think you're doing Dave?" */ ?>
 
 		<?php wp_footer(); ?>
-		<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-64232208-1', 'auto');
-  ga('send', 'pageview');
+<!-- BEGIN Analytics -->
+<script type="text/javascript">
+    var _gaq = _gaq || [];
 
+    _gaq.push(['_setAccount', 'UA-64232208-1']);
+    _gaq.push(['_setDomainName', 'hcmfo3club.net']);
+    _gaq.push(['_setAllowLinker', true]);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
 </script>
 </body>
 </html>
